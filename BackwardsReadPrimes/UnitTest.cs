9a077ed957259d0b8ca41cc259fec4020a27bdea
katrina <inlove0131@hotmail.com>
2018-01-24 22:11:26 +0800

    generate number list

--- BackwardsReadPrimes/UnitTest.cs
+++ BackwardsReadPrimes/UnitTest.cs
@@ -51,13 +51,49 @@ namespace BackwardsReadPrimes
             var expect = true;
             Assert.AreEqual(expect, result);
         }
+
+        [TestMethod]
+        public void Input1_and_1_Return1()
+        {
+            var input1 = 1;
+            var input2 = 1;
+            var result = BackwardsReadPrimes.Numers(input1, input2); ;
+            var expect = new List<int>(){1};
+            CollectionAssert.AreEqual(expect, result);
+        }
+
+        [TestMethod]
+        public void Input1_and_2_Return1and2()
+        {
+            var input1 = 1;
+            var input2 = 2;
+            var result = BackwardsReadPrimes.Numers(input1, input2); ;
+            var expect = new List<int>() { 1, 2 };
+            CollectionAssert.AreEqual(expect, result);
+        }
+
+        [TestMethod]
+        public void Input1_and_4_Return1to4()
+        {
+            var input1 = 1;
+            var input2 = 4;
+            var result = BackwardsReadPrimes.Numers(input1, input2); ;
+            var expect = new List<int>() { 1, 2, 3, 4 };
+            CollectionAssert.AreEqual(expect, result);
+        }
     }
 
     public class BackwardsReadPrimes
     {
-        public static List<int> Numers()
+        //找出一個區間所有的 prime，而且這個 prime 正反的數字都要是 prime 並且不可以是回文
+        public static List<int> Numers(int start, int end)
         {
-            return new List<int>();
+            var numbers = new List<int>();
+            for (int i = start; i <= end; i++)
+            {
+                numbers.Add(i);
+            }
+            return numbers;
         }
 
         public static bool IsPrime(int param)
